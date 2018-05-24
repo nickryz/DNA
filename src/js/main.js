1e817b9fc7719f92c20cf2b6e9af1c03df178d7f
nickryz <nickryz87@gmail.com>
2018-05-24 23:47:22 +0300

    tablet + mobile + compress img

--- src/js/main.js
+++ src/js/main.js
@@ -5,7 +5,32 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init () {
     
-alert('dsf')
+    document.getElementById('menu-toggle').addEventListener('click', function (e) {
+        let navWrap = document.querySelector('.nav-wrap');
+        
+        this.classList.toggle('open');
+        navWrap.classList.toggle('show');
+
+        if (navWrap.classList.contains('show')) {
+            document.body.style.overflow = 'hidden';
+            document.querySelector('.overlay').style.display = 'block';
+        } else {
+            document.body.style.overflow = 'auto';
+            document.querySelector('.overlay').style.display = 'none';
+        }
+    })
+
+    document.querySelector('.overlay').addEventListener('click', function (e) {
+        let navWrap = document.querySelector('.nav-wrap');
+
+        if (navWrap.classList.contains('show')) {
+            navWrap.classList.remove('show');
+            document.body.style.overflow = 'auto';
+            document.getElementById('menu-toggle').classList.remove('open');
+            document.querySelector('.overlay').style.display = 'none';
+        }
+    })
+        
 
 }
 
