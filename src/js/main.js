6075f59f1b95d3cff377de47b68274062107cdba
nickryz <nickryz87@gmail.com>
2018-05-25 12:11:13 +0300

    some fixes for scroll in js

--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,7 @@ window.addEventListener('DOMContentLoaded', init);
 // import $ from 'jquery';
 
 function init () {
-    
+    let hh;
     document.getElementById('menu-toggle').addEventListener('click', function (e) {
         let navWrap = document.querySelector('.nav-wrap');
         
@@ -12,27 +12,49 @@ function init () {
         navWrap.classList.toggle('show');
 
         if (navWrap.classList.contains('show')) {
-            document.body.style.overflow = 'hidden';
+            document.addEventListener('mousewheel', stopScrollWheel);
+            document.addEventListener('keydown', stopScrollKey);
+            document.addEventListener('touchmove', stopScrollTouch, { passive: false });
             document.querySelector('.overlay').style.display = 'block';
         } else {
-            document.body.style.overflow = 'auto';
+            document.removeEventListener('mousewheel', stopScrollWheel);
+            document.removeEventListener('keydown', stopScrollKey);
+            document.removeEventListener('touchmove', stopScrollTouch, { passive: false });
             document.querySelector('.overlay').style.display = 'none';
         }
     })
 
-    document.querySelector('.overlay').addEventListener('click', function (e) {
+    document.querySelector('.overlay').addEventListener('click', hideMenu);
+    window.addEventListener('scroll', hideMenu);
+
+    function hideMenu() {
         let navWrap = document.querySelector('.nav-wrap');
 
         if (navWrap.classList.contains('show')) {
             navWrap.classList.remove('show');
-            document.body.style.overflow = 'auto';
             document.getElementById('menu-toggle').classList.remove('open');
+            document.removeEventListener('mousewheel', stopScrollWheel);
+            document.removeEventListener('keydown', stopScrollKey);
+            document.removeEventListener('touchmove', stopScrollTouch, { passive: false });
             document.querySelector('.overlay').style.display = 'none';
         }
-    })
-        
+    }
 
-}
+    function stopScrollWheel(e) {
+        e.preventDefault();
+    }
+    
+    function stopScrollKey(e) {
+        let key = e.keyCode;
+        if (key === 33 || key === 34 || key === 40 || key === 38) {
+            e.preventDefault();
+        }
+    }
+    
+    function stopScrollTouch(e) {
+        e.preventDefault();
+    }
+    }
 
 
 
